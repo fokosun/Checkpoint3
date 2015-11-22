24e284f90bafac60b3bd7d019ccfb65b2185bda8
andela-fokosun <okosunuzflorence@gmail.com>
2015-11-22 11:43:07 +0100

    exception class

---
+++ src/Exceptions/RecordDoesNotExistException.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by Florence Okosun.
+ * Project: Checkpoint Two
+ * Date: 11/3/2015
+ * Time: 3:48 PM
+ */
+
+namespace Florence;
+
+use Exception;
+
+class RecordDoesNotExistException extends Exception
+{
+    protected $message;
+
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+    * @return Exception Message
+    */
+    public function getExceptionMessage()
+    {
+        return $this->message;
+    }
+}
