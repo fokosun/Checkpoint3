6ca31ac6055c962427b27a4733397761841d9bca
andela-fokosun <okosunuzflorence@gmail.com>
2015-12-03 15:22:26 +0100

    implement tokenn expiry

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -127,22 +127,41 @@ class AuthController {
             $user = User::where('token', $token)->first();
             if (! empty($user)) {
                 if ($user['password'] === $password && $user['username'] === $username) {
-                    $status = json_encode(['status'=>200,
-                        'username'  =>  $user['username'],
-                        'password'  =>  $user['password'],
-                        'token' => $user['token'],
-                        'token_expire' => $user['token_expire']
-                        ]);
+                    $expiry = self::isTokenExpired($token);
+                        if($expiry == true) {
+                            $status = json_encode(['status'=>301,'message'=>'session expired!']);
+                            // die();
+                        } else {
+                            $status = json_encode(['status'=>200,
+                            'username'  =>  $user['username'],
+                            'password'  =>  $user['password'],
+                            'token' => $user['token'],
+                            'token_expire' => $user['token_expire']
+                            ]);
+                        }
                 } else {
                     $status = json_encode(['status'=>404,'message'=>'Invalid credentials!']);
                 }
             } else {
-                $status = json_encode(['status'=> 404,'message' => 'Invalid credentials!']);
+                $status = json_encode(['status'=> 500,'message' => 'Error processing request!']);
             }
         } catch(QueryException $e) {
-            $response->body(json_encode(['status' => 401, 'message' => 'Invalid credentials!']));
+            $response->body(json_encode(['status' => 404, 'message' => 'Invalid credentials!']));
         }
         return $status;
+    }
+
+    public function isTokenExpired($token)
+    {
+        $user = User::where('token', $token)->first();
+
+        $token_expire = $user['token_expire'];
+        $currTime = date('Y-m-d H:i:s');
+
+        if($token_expire < $currTime) {
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -172,7 +191,7 @@ class AuthController {
         $response = $app->response();
         $response->headers->set('Content-Type', 'application/json');
 
-        $token   = $app->request->headers->get('Authorization');
+        $token = $app->request->headers('Authorization');
 
         try {
             $destroy = User::where('token', $token)
