e88ee02ebe0238d7bd65496f43ea516b71ea44c4
andela-fokosun <okosunuzflorence@gmail.com>
2015-12-03 18:40:26 +0100

    install guzzle http to test routes

---
+++ tests/EmojiRestApiTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Florence\Test;
+
+use GuzzleHttp\Client;
+
+class EmojiRestApiTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetRoutes()
+    {
+        $client     = new Client();
+        $index      = 'http://localhost:4190';
+        $allEmojis  = 'http://localhost:4190/emojis';
+
+        $index_req  =  $client->request('GET', $index);
+        $all_req    =  $client->request('GET', $allEmojis);
+
+        $this->assertEquals('200', $index_req->getStatusCode());
+        $this->assertEquals('200', $all_req->getStatusCode());
+    }
+}
