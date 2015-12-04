c126d2df8f6efc69498d03e09c936ddc04254583
andela-fokosun <okosunuzflorence@gmail.com>
2015-12-04 14:37:33 +0100

    chore

--- index.php
+++ index.php
@@ -20,17 +20,6 @@ $app = new Slim([
     'debug'          => true
 ]);
 
-// Prepare view
-$app->view(new \Slim\Views\Twig());
-$app->view->parserOptions = array(
-  'charset' 	=> 'utf-8',
-  'cache' 		=> realpath('templates/cache'),
-  'auto_reload' => true,
-  'strict_variables' => false,
-  'autoescape' 	=> true,
-);
-$app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
-
 $app->get('/', function() use ($app) {
     $app->render('index.html');
 });
