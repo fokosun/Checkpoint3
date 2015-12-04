7beab51dbe1bc93214f9e732aa5f7f5f36502bff
andela-fokosun <okosunuzflorence@gmail.com>
2015-12-04 13:23:59 +0100

    setting up heroku

--- tests/EmojiRestApiTest.php
+++ tests/EmojiRestApiTest.php
@@ -10,7 +10,7 @@ class EmojiRestApiTest extends \PHPUnit_Framework_TestCase
     {
 
         $client = new Client();
-        $res = $client->request('GET', 'http://localhost:4190');
+        $res = $client->request('GET', 'http://localhost:8080');
 
         $this->assertEquals('200', $res->getStatusCode());
 
