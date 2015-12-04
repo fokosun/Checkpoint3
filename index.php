db46247dfbc70381cd3d017f997bb6f6c6d0e20f
andela-fokosun <okosunuzflorence@gmail.com>
2015-12-04 15:25:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 * Author: Florence Okosun
 */
 
-require "vendor/autoload.php";
-//require "src/connections/Connection.php";
+require_once('vendor/autoload.php');
+require_once('src/connections/Connection.php') ;
 
 use Slim\Slim;
 use Florence\User;
