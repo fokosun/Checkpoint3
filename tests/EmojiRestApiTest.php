964d72395ff2a0e2e99a7edb4d13e2e770725058
andela-fokosun <okosunuzflorence@gmail.com>
2015-12-05 15:22:48 +0100

    testing with guzzle http

--- tests/EmojiRestApiTest.php
+++ tests/EmojiRestApiTest.php
@@ -2,27 +2,78 @@
 
 namespace Florence\Test;
 
+use Florence\Emoji;
 use Florence\Config;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 
 class EmojiRestApiTest extends \PHPUnit_Framework_TestCase
 {
-    protected $domain;
+    protected $emoji;
     protected $client;
+    protected $url;
     protected $token;
 
-    public function setUp() {
-
+    public function setUp ()
+    {
         Config::loadenv();
 
-        $this->domain = 'http://emojis4devs.herokuapp.com';
+        $this->token = getenv('TEST_TOKEN');
+        $this->emoji = new Emoji();
         $this->client = new Client();
-        $this->token = getenv('token');
+        $this->url = "http://emojis4devs.herokuapp.com";
     }
 
+    /**
+    * @expectedException GuzzleHttp\Exception\ClientException
+    */
+    public function testInvalidEndpoint()
+    {
+        $request = $this->client->request('GET', $this->url.'/auth/emojis');
+    }
+
+    /** protected route
+    * tests registration endpoint
+    */
+    public function testPostRegister()
+    {
+        $register = $this->client->request('POST', $this->url.'/register');
+        $content = $register->getHeader('content-type')[0];
+
+        $this->assertEquals('200', $register->getStatusCode());
+        $this->assertInternalType('object', $register->getBody());
+        $this->assertEquals('application/json', $content);
+    }
+
+    /**
+    * tests for all unprotected GET routes
+    */
     public function testGetRoutes()
     {
-        $res = $this->client->request('GET', $this->domain);
-        $this->assertEquals('200', $res->getStatusCode());
+        $index = $this->client->request('GET', $this->url);
+        $emojis = $this->client->request('GET', $this->url.'/emojis');
+
+        $contentTypeForIndex = $index->getHeader('content-type')[0];
+        $contentTypeForEmojis = $emojis->getHeader('content-type')[0];
+
+        $this->assertEquals('200', $index->getStatusCode());
+        $this->assertInternalType('object', $index->getBody());
+
+        $this->assertEquals('200', $emojis->getStatusCode());
+        $this->assertInternalType('object', $emojis->getBody());
+
+        $this->assertEquals('text/html;charset=UTF-8', $contentTypeForIndex);
+        $this->assertEquals('application/json', $contentTypeForEmojis);
+
     }
+
+    /**
+    *
+    */
+    public function testFindEmojiById()
+    {
+
+    }
+
+
 }
